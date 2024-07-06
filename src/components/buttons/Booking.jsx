9a5c744b09550dfce98a2cb0eb237510bb90a995
anthilldev <anttechtreejr@gmail.com>
2024-07-06 17:30:02 -0500

    initial commit

--- src/components/buttons/Booking.jsx
+++ src/components/buttons/Booking.jsx
@@ -13,7 +13,7 @@ const Booking = () => {
 
   return (
     
-    <Link href='https://www.upwork.com/fl/~0190b9c73f1f2fdff2?s=1044578476142100494'>
+    <Link href='&apos;https://www.upwork.com/fl/~0190b9c73f1f2fdff2?s=1044578476142100494'>
     <button className="p-2 bg- mr-5 rounded-lg" > <ColorChangingText text="Book Me Now" /></button>
     </Link>
     
